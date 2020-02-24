8cbae0c759349fa0c7b04df11aa7a7175ce85e46
Faran42 <faranorphys@gmail.com>
2020-02-24 12:10:21 -0300

    User model funcionando

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    "class-nethods-use-this": "off",
+    "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
-  username: 'fastfeet',
+  username: 'postgres',
   password: 'fastfeet',
-  database: 'fastfeet',
+  database: 'FastFeet',
   define: {
     timestamps: true,
     underscored: true,
