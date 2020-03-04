e213491fcff821c14f808b58d37bc64c7eadd1a6
Faran42 <faranorphys@gmail.com>
2020-03-04 00:16:54 -0300

    chore: Autenticação funcionando.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,10 +22,25 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
     }
 
-    return res.json({ ok: true });
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
